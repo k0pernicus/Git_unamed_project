d04d22a0f1dd228978dfc4e2cee2e736842ed70d
k0pernicus <antonin.carette@gmail.com>
2015-09-24 00:09:16 +0200

    Debugging

--- src/lib/git/commands.py
+++ src/lib/git/commands.py
@@ -8,10 +8,11 @@ def list_git_projects():
         if entry[-1] == '\n':
             entry = entry[:-1]
         repo = Repo(entry)
-        current_status = "CLEAN"
         if repo.is_dirty():
             current_status = "DIRTY"
-        if (not repo.is_dirty()) and (not "nothing to commit" in repo.git.status()):
+        elif not "nothing to commit" in repo.git.status():
             current_status = "TO PUSH"
+        else:
+            current_status = "CLEAN"
         to_add = "--" if not lib.settings.settings.ARGS.stats else "-- {0} commits --".format(len(list(repo.iter_commits())))
         print("\t[{0}] {1} {2} {3} untracked files".format(current_status, entry, to_add, len(repo.untracked_files)))
