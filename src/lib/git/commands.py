528e165df29ffd82de33431ec273d90d457f87a4
k0pernicus <antonin.carette@gmail.com>
2015-09-23 23:55:33 +0200

    New functionalities in list_git_projects

--- src/lib/git/commands.py
+++ src/lib/git/commands.py
@@ -1,7 +1,13 @@
 import lib.settings.settings
+from git import *
 
 def list_git_projects():
     print("List of git project:")
     #end="" -> avoid last '\n' character
     for entry in lib.settings.settings.CONFIG_FILE_CONTENT:
-        print("\t{0}".format(entry), end="")
+        if entry[-1] == '\n':
+            entry = entry[:-1]
+        repo = Repo(entry)
+        current_status = "DIRTY" if repo.is_dirty() else ("CLEAN" if "nothing to commit" in repo.git.status() else "TO PUSH")
+        to_add = "--" if not lib.settings.settings.ARGS.stats else "-- {0} commits --".format(len(list(repo.iter_commits())))
+        print("\t[{0}] {1} {2} {3} untracked files".format(current_status, entry, to_add, len(repo.untracked_files)))
