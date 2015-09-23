e6b051d1d93edc6dea83e88b7f22fc315aec4e43
k0pernicus <antonin.carette@gmail.com>
2015-09-23 22:02:12 +0200

    New functions to add and delete new entries

--- src/lib/config/config.py
+++ src/lib/config/config.py
@@ -1,7 +1,7 @@
 import lib.settings.settings
 
 def open_config_file():
-    return open(lib.settings.settings.CONFIG_FILE_PATH, "wr")
+    return open(lib.settings.settings.CONFIG_FILE_PATH, "r+")
 
 def close_config_file(f):
     try:
@@ -16,8 +16,20 @@ def clean_config_file(f):
     except:
         print("The file {0} canno't be truncate, and close".format(f.name))
 
-def add_new_path(f, path):
+def add_new_path(f, path_to_add):
     try:
-        f.write("{0}\n".format(path))
+        f.write("{0}\n".format(path_to_add))
     except:
-        print("The path {0} canno't be added to {1}".format(path, f.name))
+        print("The path {0} canno't be added to {1}".format(path_to_add, f.name))
+
+def delete_new_path(f, path_to_delete):
+    try:
+        entries = f.readlines()
+        f.seek(0)
+        for entry in entries:
+            if entry != path_to_delete:
+                f.write(entry)
+        f.truncate()
+        f.close()
+    except:
+        print("Impossible to delete the path {0} from file {1}".format(path_to_delete, f.name))
