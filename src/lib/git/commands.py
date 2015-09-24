860d9e271743f53e9740e405a5b528f18f20010a
k0pernicus <antonin.carette@gmail.com>
2015-09-24 19:57:25 +0200

    Debugging

--- src/lib/git/commands.py
+++ src/lib/git/commands.py
@@ -12,7 +12,7 @@ def push_ready_projects():
     any_repo_to_push = False
     for git_project in lib.settings.settings.GIT_OBJECTS:
         if git_project.current_status == "TO PUSH":
-            git_project.git_object.remote.push()
+            git_project.git_object.remote().push()
             print("Pushing for {0}...".format(git_project.entry))
             any_repo_to_push = True
     if not any_repo_to_push:
