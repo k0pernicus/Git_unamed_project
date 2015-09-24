fc4c1887cddd9f45c048cf91f9bdf181cb7749f8
k0pernicus <antonin.carette@gmail.com>
2015-09-24 19:55:07 +0200

    Simple boolean to know if there's exists repository to push

--- src/lib/git/commands.py
+++ src/lib/git/commands.py
@@ -9,7 +9,11 @@ def list_git_projects():
 
 def push_ready_projects():
     print("Repository to push...")
+    any_repo_to_push = False
     for git_project in lib.settings.settings.GIT_OBJECTS:
         if git_project.current_status == "TO PUSH":
             git_project.git_object.remote.push()
             print("Pushing for {0}...".format(git_project.entry))
+            any_repo_to_push = True
+    if not any_repo_to_push:
+        print("There is no repository to push yet!")
