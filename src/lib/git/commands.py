90c088e5486d79f1b1c7aa2f80d2af7e4dfa2391
k0pernicus <antonin.carette@gmail.com>
2015-09-24 19:45:40 +0200

    Simplification of the code and new functionality : to push a project

--- src/lib/git/commands.py
+++ src/lib/git/commands.py
@@ -4,15 +4,12 @@ from git import *
 def list_git_projects():
     print("List of git project:")
     #end="" -> avoid last '\n' character
-    for entry in lib.settings.settings.CONFIG_FILE_CONTENT:
-        if entry[-1] == '\n':
-            entry = entry[:-1]
-        repo = Repo(entry)
-        if repo.is_dirty():
-            current_status = "DIRTY"
-        elif not "Your branch is up-to-date" in repo.git.status():
-            current_status = "TO PUSH"
-        else:
-            current_status = "CLEAN"
-        to_add = "--" if not lib.settings.settings.ARGS.stats else "-- {0} commits --".format(len(list(repo.iter_commits())))
-        print("\t[{0}] {1} {2} {3} untracked files".format(current_status, entry, to_add, len(repo.untracked_files)))
+    for git_object in lib.settings.settings.GIT_OBJECTS:
+        print(git_object)
+
+def push_ready_projects():
+    print("Repository to push...")
+    for git_project in lib.settings.settings.GIT_OBJECTS:
+        if git_project.current_status == "TO PUSH":
+            git_project.git_object.remote.push()
+            print("Pushing for {0}...".format(git_project.entry))
