f791271e16e28250c7cb0bfabe3530dec4ffd80f
k0pernicus <antonin.carette@gmail.com>
2015-09-25 20:38:14 +0200

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='giwyn',
-      version='0.1',
+      version='0.2',
+      packages=find_packages(),
       description='A simple command to manage all your git clones',
       url='http://github.com/k0pernicus/giwyn',
       author='k0pernicus',
       author_email='antonin.carette@gmail.com',
       license='GnuGPL',
-      packages=['giwyn'],
       classifiers=[
-          'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Build Tools',
-          'Programming Language :: Python :: 3.4'
+          'Programming Language :: Python :: 3.4',
+          'Environment :: Console',
       ],
       keywords='git development versioning package',
-      install_requires=['gitpython'],
-      zip_safe=False)
+      install_requires=[
+        'gitpython',
+      ],
+      entry_points={
+        'console_scripts': [
+            'giwyn=giwyn.main:main'
+        ]
+      })
