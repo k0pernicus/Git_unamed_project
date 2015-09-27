7efd5225ad07bb01b71cf443a6edaa38eb939099
k0pernicus <antonin.carette@gmail.com>
2015-09-27 12:11:58 +0200

    Debugging

--- giwyn/lib/gitconf/git_o.py
+++ giwyn/lib/gitconf/git_o.py
@@ -7,7 +7,7 @@ class GitObj(object):
         self.git_object = self.get_repo_from_entry(entry)
         self.current_status = self.return_current_status()
         self.all_commits = len(list(self.git_object.iter_commits()))
-        #self.commits_to_push = self.return_nb_commits_to_push()
+        self.commits_to_push = self.return_nb_commits_to_push()
         self.untracked_files = len(self.git_object.untracked_files)
 
     def __del__(self):
@@ -15,9 +15,9 @@ class GitObj(object):
 
     def __str__(self):
         if not self.current_status == "TO PUSH":
-            return "\t[{0}] {1} -- {2} commits -- {3} untracked files".format(self.current_status, self.entry, self.all_commits, self.untracked_files)
+            return "\t[{0}] {1} -- {2} commit(s) -- {3} untracked file(s)".format(self.current_status, self.entry, self.all_commits, self.untracked_files)
         else:
-            return "\t[{0}] {1} -- {2} commits to push -- {3} untracked files".format(self.current_status, self.entry, self.commits_to_push, self.untracked_files)
+            return "\t[{0}] {1} -- {2} commit(s) to push -- {3} untracked file(s)".format(self.current_status, self.entry, self.commits_to_push, self.untracked_files)
 
     def check_entry(self, entry):
         if entry[-1] == '\n':
