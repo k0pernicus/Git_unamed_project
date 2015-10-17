3d4922f816d2deb3bef0bff6a4e4feb3e2e696c0
k0pernicus <antonin.carette@gmail.com>
2015-10-17 13:08:47 +0200

    Debugging

--- giwyn/lib/gitconf/git_o.py
+++ giwyn/lib/gitconf/git_o.py
@@ -41,7 +41,7 @@ class GitObj(object):
     def return_nb_commits_to_push(self):
         git_status = self.git_object.git.status()
         if self.current_status == "TO PUSH":
-            if not "Your branch is ahead of":
+            if "Your branch is ahead of" in git_status:
                 split_git_status_0 = git_status.split("commit")[0] or git_status.split("commits")[0]
                 split_git_status_0 = split_git_status_0.split('by')[1]
                 return int(split_git_status_0)
