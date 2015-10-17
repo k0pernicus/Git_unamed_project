8bed10cffc18461f5223d7e1d6e3da29701af160
k0pernicus <antonin.carette@gmail.com>
2015-10-17 13:19:06 +0200

    Modification of typo

--- giwyn/lib/gitconf/commands.py
+++ giwyn/lib/gitconf/commands.py
@@ -12,7 +12,7 @@ def push_ready_projects():
     any_repo_to_push = False
     for git_project in giwyn.lib.settings.settings.GIT_OBJECTS:
         if git_project.current_status == "TO PUSH":
-            print("Pushing for {0}...".format(git_project.entry))
+            print("Pushing {0}...".format(git_project.entry))
             git_project.git_object.remote().push()
             any_repo_to_push = True
     if not any_repo_to_push:
